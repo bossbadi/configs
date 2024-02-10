520eee8653b900131868f9ff3cff892ed2a1f99f
Andy Tang <bossbadi123@gmail.com>
2024-02-10 12:03:53 -0500

    add setup suroi server helper script

---
+++ Suroi/setup-suroi-server.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script to install a single Suroi.io server
+
+setup_server() {
+    # download pm2 config
+    cd /apps/suroi-$1/
+    curl -O https://raw.githubusercontent.com/bossbadi/configs/master/pm2/suroi-server-1/ecosystem.config.js
+    sed -i "s/1/$1/g" ecosystem.config.js
+
+    # download server config
+    cd server/src/
+    curl -O https://raw.githubusercontent.com/bossbadi/configs/master/Suroi/server-$1/config.ts
+    cd data/
+    curl -O -f -s https://raw.githubusercontent.com/bossbadi/configs/master/Suroi/server-$1/data/maps.ts
+    curl -O -f -s https://raw.githubusercontent.com/bossbadi/configs/master/Suroi/server-$1/data/lootTables.ts
+
+    # build server
+    cd /apps/suroi-$1/server/
+    pnpm install
+    pnpm build
+
+    # start server
+    cd /apps/suroi-$1/
+    pm2 start
+}
+
+num=$1
+
+if ! [[ $num =~ ^[0-9]+$ ]]; then
+    echo "Error: first argument must be a number"
+    exit 1
+fi
+
+setup_server $num
