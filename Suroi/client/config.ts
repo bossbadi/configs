269bc8b04030d567665f38d23f8c276ff30d9c5e
Andy Tang <bossbadi123@gmail.com>
2024-02-10 02:40:36 -0500

    update suroi region names

--- Suroi/client/config.ts
+++ Suroi/client/config.ts
@@ -1,22 +1,19 @@
 export const Config = {
   regions: {
-    main: { name: "Main", address: "suroi-1.gopubby.com", https: true },
-    savannah: { name: "Savannah", address: "suroi-2.gopubby.com", https: true },
-    docks: { name: "Docks", address: "suroi-3.gopubby.com", https: true },
+      1: { name: "Downtown", address: "suroi-1.gopubby.com", https: true },
+      2: { name: "Sniper's Lair", address: "suroi-2.gopubby.com", https: true },
+      3: { name: "Ricochet Arena", address: "suroi-3.gopubby.com", https: true },
   },
-  defaultRegion: "main",
-  mode: "normal",
+  defaultRegion: "1",
+  mode: "normal"
 } satisfies ConfigType as ConfigType;
 
 export interface ConfigType {
-  readonly regions: Record<
-    string,
-    {
-      readonly name: string;
-      readonly address: string;
-      readonly https: boolean;
-    }
-  >;
-  readonly defaultRegion: string;
-  readonly mode: string;
+  readonly regions: Record<string, {
+      readonly name: string
+      readonly address: string
+      readonly https: boolean
+  }>
+  readonly defaultRegion: string
+  readonly mode: string
 }
