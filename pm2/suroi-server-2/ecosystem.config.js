5a24784505b933c3cdfaa57bfb8e2f3586b370c3
Andy Tang <bossbadi123@gmail.com>
2024-02-10 01:55:54 -0500

    remove unneeded files

--- pm2/suroi-server-2/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "suroi-server-2",
-      script: "cd /apps/suroi-2/server && pnpm start",
-    },
-  ],
-};
--- pm2/suroi-server-3/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "suroi-server-3",
-      script: "cd /apps/suroi-3/server && pnpm start",
-    },
-  ],
-};
